039417e59eeb00b8a575da1ce91127cbc48d5f19
fabio <fabiounipar@gmail.com>
2020-03-04 19:54:25 -0300

    Adicionado consulta com paginação

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -71,6 +71,10 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		Categoria cat3 = new Categoria(null, "Casa");
+		Categoria cat4 = new Categoria(null, "Jardinagem");
+		Categoria cat5 = new Categoria(null, "Marcenaria");
+		Categoria cat6 = new Categoria(null, "Eletronicos");
+		Categoria cat7 = new Categoria(null, "Decoração");
 		
 		Produto p1 = new Produto(null, "Teclado", 50.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -80,13 +84,15 @@ public class CursomcApplication implements CommandLineRunner{
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2));
 		cat2.getProdutos().addAll(Arrays.asList(p3));
 		cat3.getProdutos().addAll(Arrays.asList(p4));
+		cat3.getProdutos().addAll(Arrays.asList(p4));
+		
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat2));
 		p4.getCategorias().addAll(Arrays.asList(cat3));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
 		
 		///////////////////////////////////////////////////////
--- src/main/java/com/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursomc/services/CategoriaService.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursomc.domain.Categoria;
@@ -48,7 +52,9 @@ public class CategoriaService {
 			categoriaRepository.deleteById(id);
 			
 		} catch (DataIntegrityViolationException dive) {
+			
 			throw new IntegrityViolationException("Nao é possivel deletar uma categoria que contem produtos.");
+			
 		}
 		
 	}
@@ -56,6 +62,14 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page , linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+		
+ 		return categoriaRepository.findAll(pageRequest);
+		
 	}
 
 }
